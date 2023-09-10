62243cbcfb75042ec2d3b42c05267692cc5a23c3
Muhammad Haddi <104686607+MHaddi1@users.noreply.github.com>
2023-09-09 20:38:49 -0500

    create container styles beats

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -9,12 +9,63 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgDarkColor,
       appBar: AppBar(
+          backgroundColor: bgDarkColor,
+          leading: const Icon(
+            Icons.sort_rounded,
+            color: whiteColor,
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: whiteColor,
+                ))
+          ],
           title: ourStyle(
-              family: GoogleFonts.openSansTextTheme,
+              family: GoogleFonts.openSansTextTheme(),
               color: whiteColor,
               size: 18)),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            itemCount: 15,
+            itemBuilder: (context, index) {
+              return Container(
+               
+                  margin: const EdgeInsets.only(bottom: 4.0),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(12.0)),
+                  child:  ListTile(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0)
+                    ),
+                    tileColor: bgColor,
+                    title: const Text(
+                      "Music Name",
+                      style: TextStyle(fontSize: 15.0, color: whiteColor),
+                    ),
+                    subtitle: const Text(
+                      "Artist Name",
+                      style: TextStyle(fontSize: 15.0, color: whiteColor),
+                    ),
+                    leading: const Icon(
+                      Icons.music_note,
+                      color: whiteColor,
+                      size: 32,
+                    ),
+                    trailing: const Icon(
+                      Icons.play_arrow,
+                      color: whiteColor,
+                      size: 26.0,
+                    ),
+                  ));
+            }),
+      ),
     );
   }
 }
